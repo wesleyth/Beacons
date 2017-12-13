245c3fd01e02a552ab46e4080180a12c59b102a4
jeroensmink98 <jeroensmink98@gmail.com>
2017-12-13 12:01:50 +0100

    Added Post ding

--- app/src/main/java/nl/alledaags/lightcurbsdktestapp/activity/LoginActivity.java
+++ app/src/main/java/nl/alledaags/lightcurbsdktestapp/activity/LoginActivity.java
@@ -5,7 +5,10 @@ import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.loopj.android.http.*;
+import org.json.*;
 
+import cz.msebera.android.httpclient.Header;
 import nl.alledaags.lightcurbsdktestapp.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,5 +22,19 @@ public class LoginActivity extends AppCompatActivity {
     public void onStartClick(View view) {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post("http://138.102.190.179/api/openday", new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+            }
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+            }
+        });
+
     }
+
 }
