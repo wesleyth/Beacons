c201f84d02ab5ff0ae682a4c6f3512f665a47b1e
wesley <terhortw@gmail.com>
2017-12-13 11:45:57 +0100

    fix1

--- app/src/main/java/nl/alledaags/lightcurbsdktestapp/activity/LoginActivity.java
+++ app/src/main/java/nl/alledaags/lightcurbsdktestapp/activity/LoginActivity.java
@@ -1,9 +1,12 @@
 package nl.alledaags.lightcurbsdktestapp.activity;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import nl.alledaags.lightcurbsdktestapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
